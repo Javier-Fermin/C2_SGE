3dc292029414468d8a077383c5da202161d3f9b5
ysasgon <fdeysasigonzalez@gmail.com>
2024-01-31 17:48:46 +0100

    tournaments 2

--- esport_six/models/tournament.py
+++ esport_six/models/tournament.py
@@ -1,4 +1,7 @@
-from odoo import api,models,fields
+import datetime
+
+from odoo import api,models,fields,exceptions
+from datetime import date
 
 class Tournament(models.Model):
     _name = 'esports.tournament'
@@ -9,3 +12,26 @@ class Tournament(models.Model):
     best_of = fields.Integer(string='Best Of')
     date = fields.Date(string='Date')
     matches = fields.One2many('esport_six.match', 'tournament_id', string='Matches')
+
+    @api.onchange('best_of')
+    def _check_numeric_values(self):
+        for record in self:
+            if record.best_of < 1:
+                raise exceptions.ValidationError("The tournament best of value must be more than or equal to 1.")
+
+    #constraint de date format
+    @api.constrains
+    def _onchange_date(self):
+        if self.date.null():
+            raise exceptions.ValidationError("The date field must be informed, the selected date is null.")
+
+    #api.model create & write
+    @api.model
+    def create(self, vals):
+        if 'date' not in vals:
+            vals['date'] = date.today()
+        return super(Tournament, self).create(vals)
+
+    @api.model
+    def write(self, vals):
+        return super(Tournament, self).write(vals)
